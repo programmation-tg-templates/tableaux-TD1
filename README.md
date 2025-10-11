91264bb8bc1aaf593fe8480ace488fa8c220da0a
François Roland <francois.roland@heh.be>
2025-10-11 19:27:53 +0200

    Update README and add unit tests for somme function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# base
+# Tableaux - TD1 - La somme
 
-Consignes
+## Consignes
+
+Écrire une fonction `somme` qui prend en paramètre un tableau de nombres et retourne la somme des éléments du tableau.
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,24 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import { somme } from "../index";
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
-    });
+describe("La fonction somme", () => {
+  test("avec un tableau vide", () => {
+    expect(somme([])).toBe(0);
+  });
+  test("avec un tableau de nombres positifs", () => {
+    expect(somme([1, 2, 3, 4, 5])).toBe(15);
+  });
+  test("avec un tableau de nombres négatifs", () => {
+    expect(somme([-1, -2, -3, -4, -5])).toBe(-15);
+  });
+  test("avec un tableau de nombres mixtes", () => {
+    expect(somme([-1, 2, -3, 4, -5])).toBe(-3);
+  });
+  test("avec un tableau contenant des zéros", () => {
+    expect(somme([0, 0, 0, 0])).toBe(0);
+  });
+  test("avec un tableau contenant un seul élément", () => {
+    expect(somme([42])).toBe(42);
+  });
 });
